c8c726626af7404c344c2f691e488bf4713781c4
Naveen kumar <naveenpuzhayanthi@gmail.com>
2026-08-25 01:10:52 +0530

    Modified with new backend files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,9 +89,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "tata_showroom",
+        "USER": "root",
+        "PASSWORD": "Naveen934599",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
